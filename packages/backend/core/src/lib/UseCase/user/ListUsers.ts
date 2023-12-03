7ff54823a9a47e5230122533959c9578767bad43
jrodriguez <jrodriguez@novosit.com>
2023-12-03 02:17:21 -0400

    add documentation

--- packages/backend/core/src/lib/UseCase/user/ListUsers.ts
+++ packages/backend/core/src/lib/UseCase/user/ListUsers.ts
@@ -1,14 +1,30 @@
 import { IUserRepository, Userview } from "@prodoctivity-dashboard/models";
 
 
-
+/**
+ * List users use case
+ *
+ * @export
+ * @class ListUsers
+ */
 export class ListUsers {
     userRepository: IUserRepository;
 
+    /**
+     * Creates an instance of ListUsers.
+     * @param {IUserRepository} userRepository
+     * @memberof ListUsers
+     */
     constructor(userRepository: IUserRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * perform the use case
+     *
+     * @return {*}  {Promise<Userview[]>}
+     * @memberof ListUsers
+     */
     async execute(): Promise<Userview[]> {
         const users = (await this.userRepository.list()).map((user) => {
             return {
@@ -20,5 +36,4 @@ export class ListUsers {
         });
         return users;
     }
-
 }
